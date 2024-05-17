12164b0fe8f56fdd43690c2c26c425d37923e437
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2024-05-17 15:18:08 +0700

    setup mongo db connection

--- ms-iqbalbaihaqi-betest/app.js
+++ ms-iqbalbaihaqi-betest/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connect = require('./connection/mongodb');
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,9 @@ app.use(
     extended: true,
   })
 )
+
+connect();
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
